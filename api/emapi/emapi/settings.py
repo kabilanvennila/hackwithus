008402b35d787846fe2a26acda9210c32f5a5162
kabilanvennila <kabilanvennila@gmail.com>
2021-10-14 13:17:56 +0530

    commit

--- api/emapi/emapi/settings.py
+++ api/emapi/emapi/settings.py
@@ -125,18 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
 STATIC_URL = '/static/'
 
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-MEDIA_URL='/media/'
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static')
+STATIC_DIRS=[
+    os.path.join(BASE_DIR,'static')
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
