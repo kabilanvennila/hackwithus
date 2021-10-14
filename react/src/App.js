93af6d28635efe6951b0b71cb0d2420368f5c114
Akash <gsaisriram.akash@gmail.com>
2021-10-14 17:14:59 +0530

    final

--- react/src/App.js
+++ react/src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
 import React, { useState } from "react";
+import axios from "axios";
+import ResultCard from "./ResultCard";
 
 function App() {
-  let [hash, setHash] = useState("");
+  let [hash, setHash] = useState("Null");
+  let [gas, setGas] = useState("Null");
+  let [carbon, setCarbon] = useState("Null");
 
   function fetchDetails(e) {
     e.preventDefault();
     let enteredHash = e.target[1].value;
-    if (enteredHash === "0x5dcfa3136bf47ec85d6510672fc762d7a21dff3f") {
-      console.log("Match");
-      setHash("Match");
-    }
+    let example = "";
+
+    let url = "https://wowexi8001.pythonanywhere.com/api/Apis/";
+    axios.get(url).then((res) => {
+      if (hash === "0x5dcfa3136bf47ec85d6510672fc762d7a21dff3f") {
+        setHash(res.data[0].Address);
+        setGas(res.data[0].Gas);
+        setCarbon(res.data[0].Carbon);
+      } else if (hash === "0xe2093ba0ffec4a1513a854faa837394c33d9f1c0") {
+        setHash(res.data[2].Address);
+        setGas(res.data[2].Gas);
+        console.log(res.data[2].Gas);
+        setCarbon(res.data[2].Carbon);
+      } else if (hash === "0xa8ac2b7148e3f60492d48d63d10a0e8469b3fbde") {
+        setHash(res.data[4].Address);
+        setGas(res.data[4].Gas);
+        setCarbon(res.data[4].Carbon);
+        // console.log(res.data[4].Address);
+      }
+    });
   }
 
   return (
@@ -52,6 +72,10 @@ function App() {
             </button>
           </div>
         </form>
+
+        <br />
+        <br />
+        <ResultCard hash={hash} gas={gas} carbon={carbon} />
       </div>
     </div>
   );
---
+++ react/src/ResultCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+function ResultCard({ hash, gas, carbon }) {
+  return (
+    <div className="card__cont">
+      {/* <span className="blue">alert</span> */}
+      <div>
+      <p>Ether Hash: </p>
+          <code>{hash}</code>
+          <p>Amount of Gas Emitted : {gas}</p>
+          <p>Amount of CO2 Pollution : {carbon}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ResultCard;
